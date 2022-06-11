94fdae30e30380cc0581e60ca43691b6818243f3
unknown <ameeratwork@gmail.com>
2022-06-11 15:54:26 +0500

    Overflow vulnerability and solution is added

--- contracts/3.Overflow.sol
+++ contracts/3.Overflow.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: None
 
+// We can make use 0.8 version of solidity, which checks overflow by default
 pragma solidity ^0.6.0;
+
 
 
 contract VulnerableToken {
@@ -17,6 +19,37 @@ contract VulnerableToken {
 
     function transfer(address _to, uint256 _amount) public {
         require(balanceOf[msg.sender] - _amount >= 0, "Not enough tokens");
+        balanceOf[msg.sender] -= _amount;
+        balanceOf[_to] += _amount;
+    }
+
+    function mint(uint256 _amount) external {
+        totalSupply += _amount;
+        balanceOf[owner] += _amount;
+    }
+}
+
+/* 
+    We can use SafeMath library from openzeppelin to avoid overflows
+ */
+contract SecureToken {
+    mapping(address => uint256) public balanceOf;
+    uint256 public totalSupply;
+    address public owner;
+    constructor(uint256 _initialSupply) public {
+        totalSupply = _initialSupply;
+        balanceOf[msg.sender] = _initialSupply;
+        owner = msg.sender;
+    }
+
+
+
+    function transfer(address _to, uint256 _amount) public {
+        /* 
+            To avoid overflow, we can check if balance is greater than amount. This way
+            we don't have to substract amount, avoiding overflow.
+         */
+        require(balanceOf[msg.sender] >= _amount, "Not enough tokens");
         balanceOf[msg.sender] -= _amount;
         balanceOf[_to] += _amount;
     }
--- test/3.overflow.js
+++ test/3.overflow.js
@@ -1,47 +1,78 @@
 const { expect } = require("chai").use(require('chai-as-promised'));
 const { ethers } = require("hardhat");
 
-describe("VulnerableToken", function () {
-    let deployer, attacker, user
-  
+describe("Overflow in smart contracts", function () {
+    let deployer, attacker, user, vulnerableToken, secureToken
+
     beforeEach(async () => {
-      [deployer, attacker, user] = await ethers.getSigners()
-  
-      const VulnerableAgreedPrice = await ethers.getContractFactory('VulnerableToken')
-      vulnerableAgreedPrice = await VulnerableAgreedPrice.deploy(100)
-      await vulnerableAgreedPrice.deployed()
-  
-      const SecureAgreedPrice = await ethers.getContractFactory('SecureAgreedPrice')
-      secureAgreedPrice = await SecureAgreedPrice.deploy(100)
-      await secureAgreedPrice.deployed()
-  
-  
+        [deployer, attacker, user] = await ethers.getSigners()
+
+        const VulnerableToken = await ethers.getContractFactory('VulnerableToken')
+        vulnerableToken = await VulnerableToken.deploy(1000)
+        await vulnerableToken.deployed()
+
+          const SecureToken = await ethers.getContractFactory('SecureToken')
+          secureToken = await SecureToken.deploy(1000)
+          await secureToken.deployed()
+
+
     })
-  
-    describe('Vulnerable Agreed Price', () => {
-      it('should set value while deploying', async () => {
-        expect((await vulnerableAgreedPrice.price()).toString()).to.eq('100')
-      })
-      it('should change value when attacker attacks', async () => {
-        await vulnerableAgreedPrice.connect(attacker).updatePrice(1000)
-        expect((await vulnerableAgreedPrice.price()).toString()).to.eq('1000')
-      })
+
+    describe('VulnerableToken', () => {
+        it('should allow a user to transfer amounts smaller than or equal to its balance', async () => {
+            await vulnerableToken.transfer(user.address, 1)
+            expect(await vulnerableToken.balanceOf(user.address)).to.eq(1)
+            expect(await vulnerableToken.balanceOf(deployer.address)).to.eq(
+                (await vulnerableToken.totalSupply()) - 1
+            )
+        })
+
+        /**
+         * @desc - It should revert in secure contract, but as this contract is vulnerable, it won't revert
+         * @notice - Attacker is transfering more tokens than he has
+         */
+
+        it('Should not revert if the attacker tries to transfer an amount greater than its balance', async () => {
+            await vulnerableToken.transfer(attacker.address, 10)
+
+            await expect(vulnerableToken.connect(attacker).transfer(user.address, 11))
+                .to.not.be.revertedWith("Not enough tokens")
+        })
+
+        /**
+         * @desc - It would cause overflow because attacker is transfering more than his balance
+         * @dev - In a uint256 overflow, 10-11 would be equal to 2^256 - 1
+         * @dev - MaxUint256 is equal to 2^256 - 1
+         */
+        it('Should overflow if an attacker transfers an amount greater than its balance', async () => {
+            await vulnerableToken.transfer(attacker.address, 10)
+
+            const initialAttackerBalance = await vulnerableToken.balanceOf(attacker.address)
+            console.log(`Initial attacker balance: ${initialAttackerBalance.toString()} tokens`)
+            
+            await vulnerableToken.connect(attacker).transfer(user.address, 11)
+
+            const finalAttackerBalance = await vulnerableToken.balanceOf(attacker.address)
+            console.log(`Final attacker balance: ${finalAttackerBalance.toString()} tokens`)
+
+            expect(await vulnerableToken.balanceOf(attacker.address)).to.eq(ethers.constants.MaxUint256)
+        })
+
     })
-    describe('Secure Agreed Price', () => {
-      it('should set value while deploying', async () => {
-        expect((await secureAgreedPrice.price()).toString()).to.eq('100')
-      })
-      it('checks if deployer is the owner ', async () => {
-        expect(await secureAgreedPrice.owner()).to.eq(deployer.address)
-      })
-      it('should change value when owner changes', async () => {
-        await secureAgreedPrice.updatePrice(1000)
-        expect((await secureAgreedPrice.price()).toString()).to.eq('1000')
-      })
-      it('should not change the value when attacker attacks', async () => {
-        await expect(secureAgreedPrice.connect(attacker).updatePrice(1000)).to.be.rejectedWith(`Ownable: caller is not the owner`)
-        expect((await vulnerableAgreedPrice.price()).toString()).to.not.eq('1000')
-      })
-      
+    describe('Secure Token', () => {
+        /**
+         * 
+         * @desc - It should revert in secure contract
+         * @notice - Attacker is transfering more tokens than he has
+         * @
+         */
+
+         it('Should revert if the attacker tries to transfer an amount greater than its balance', async () => {
+            await secureToken.transfer(attacker.address, 10)
+
+            await expect(secureToken.connect(attacker).transfer(user.address, 11))
+                .to.be.revertedWith("Not enough tokens")
+        })
+
     })
-  });
+});
